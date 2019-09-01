1f96caa61ed6855df55f206761c189e73f96a900
Joshua Fuentes <joshuarpf@gmail.com>
2019-09-02 00:25:12 +0800

    initial store implementation

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,1 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import createSagaMiddleware from 'redux-saga'
+import root from './sagas'
+
+const initializeSagaMiddleware = createSagaMiddleware()
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	reducer, 
+	storeEnhancers(
+		applyMiddleware(initializeSagaMiddleware)
+	)
+)
+
+initializeSagaMiddleware.run(root)
+
+export default store
