5a9c3cabd3895b7436919d26ffad2744a88ea6a5
Joshua Fuentes <joshuarpf@gmail.com>
2019-09-02 23:27:09 +0800

    WIP implemented fetching of data

--- src/sagas/index.jsx
+++ src/sagas/index.jsx
@@ -1,14 +1,15 @@
 import { all, put , takeEvery } from 'redux-saga/effects';
 import {  SEARCH } from '../common'
 import { profileFunctions } from '../modules/profiles'
+import ReportDispatcher from 'jest-jasmine2/build/jasmine/ReportDispatcher';
 // import { DISPLAY_GROUPS, DISPLAY_REPOS, SEARCH } from '../common'
 
 function* makeAPICall(action) {
 	try {
-		
-		const repos = profileFunctions.getUserRepos(action.payload.userid)
-		console.log(repos)
-
+		profileFunctions.getUserRepos(action.payload.userid)
+		.then((testing) => {
+			console.log(testing)
+		})
 		yield put({ type: 'DISPLAY_REPOS'})
 	} catch (error) {
 		yield put({ type: 'API_ERROR', payload: error })
