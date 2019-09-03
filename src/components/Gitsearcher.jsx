a2e9ba725728fa4ec1cce40f93607698193008cc
Joshua Fuentes <joshuarpf@gmail.com>
2019-09-03 14:15:41 +0800

    WIP: Added styles

--- src/components/Gitsearcher.jsx
+++ src/components/Gitsearcher.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { searchUserData } from '../actions'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button';
 
-let searchButton = ({ searchUserData }) => (
-	<button onClick={ () => searchUserData('freeCodeCamp') }>Search</button>
-)
+const useStyles = makeStyles(theme => ({
+	container: {
+	  display: 'flex',
+	  flexWrap: 'wrap',
+	},
+	textField: {
+	  marginLeft: theme.spacing(1),
+	  marginRight: theme.spacing(1),
+	  width: 200,
+	},
+	dense: {
+	  marginTop: 19,
+	},
+	menu: {
+	  width: 200,
+	},
+	button: {
+		margin: theme.spacing(1),
+	  },
+  }));
+
+
+let searchButton = ({ searchUserData }) => {
+
+	const classes = new useStyles()
+
+	return (
+		<div>
+			<Button variant="contained" color="primary" className={classes.button} onClick={ () => searchUserData('dotslashbin') }>Search</Button>
+		</div>
+	)
+}
 
 const mapDispatchToProps = {
 	searchUserData: searchUserData
